3a1d1750a9dc1fce97eea17a40ab4b96d56fba99
Brad Deibert <bradeibert7@gmail.com>
2022-05-15 15:25:28 -0600

    feat: add Lato fonts to app

--- src/components/ResumeContent.svelte
+++ src/components/ResumeContent.svelte
@@ -18,7 +18,7 @@
 
 	<h2>Education</h2>
 	<p>
-		My education... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum
+		My education is... Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec elementum
 		tristique mattis. Phasellus mattis ultrices lorem. Aliquam quis quam augue. Nullam nec dui
 		cursus, vulputate magna ut, tristique nisl. Fusce aliquam neque vel lobortis imperdiet. Aliquam
 		erat volutpat. Mauris ligula orci, placerat et tempor a, auctor eget diam. Duis ut hendrerit
